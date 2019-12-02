ea5612ed8dc0435a5f1fdb60c3f27bd43226109a
Jesus 93 <oajesart@hotmail.com>
2019-12-02 11:25:48 -0600

    Funcionalidad de costo

--- SitioWeb/App_Start/BundleConfig.cs
+++ SitioWeb/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace SitioWeb
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                      "~/Scripts/Custom/CustomBehavior.js"));
         }
     }
 }
---
+++ SitioWeb/Scripts/Custom/CustomBehavior.js
@@ -0,0 +1,25 @@
+﻿$(".customCheck").change(function () {
+    //debugger;
+    var cost = 0.0;
+    $(".customCheck").each(function (index, element) {
+        var costValue = element.getAttribute("costo");
+        if (element.checked) {
+
+        cost += parseFloat(costValue);
+        }
+    });
+    $("#lblCostoTotal").text(cost);
+
+});
+
+$(document).ready(function () {
+    var cost = 0.0;
+    $(".customCheck").each(function (index, element) {
+        var costValue = element.getAttribute("costo");
+        if (element.checked) {
+
+            cost += parseFloat(costValue);
+        }
+    });
+    $("#lblCostoTotal").text(cost);
+});
